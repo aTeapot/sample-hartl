d82d2e15ea73bfa3f258663aa513ee886ddce62a
gentiana <latem999@o2.pl>
2014-05-30 15:11:29 +0200

    DRY specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
     before { visit '/static_pages/home' }
@@ -10,7 +12,7 @@ describe "Static pages" do
     end
     
     it "should have the right title" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   
@@ -22,7 +24,7 @@ describe "Static pages" do
     end
     
     it "should have the right title" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   
@@ -34,7 +36,7 @@ describe "Static pages" do
     end
     
     it "should have the right title" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
   
@@ -46,7 +48,7 @@ describe "Static pages" do
     end
     
     it "should have the right title" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
   
