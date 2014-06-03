8a979f1f87661ffc4bfc0ffd1040987a046626b6
gentiana <latem999@o2.pl>
2014-06-03 16:24:22 +0200

    Minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   
   has_secure_password
   
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,11 +20,10 @@ describe User do
   it { should validate_uniqueness_of(:email) }
   it { should ensure_length_of(:name).is_at_most(50) }
   it { should ensure_length_of(:password).is_at_least(6) }
-  xit { should_not allow_value('foo@bar..com').for(:email) }
   
   context "email" do
     describe "when email format is invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         it { should_not allow_value(invalid_address).for(:email) }
       end
@@ -44,6 +43,15 @@ describe User do
         user_with_same_email.save
       end
       it { should_not be_valid }
+    end
+    
+    describe "email address with mixed case" do
+      let(:mixed_case_email) { 'eMoMarTynKa@auC.pl' }
+      it "should be downcased before save" do
+        @user.email = mixed_case_email
+        @user.save
+        expect(@user.reload.email).to eq mixed_case_email.downcase
+      end
     end
   end
   
