d6a40b14a03529c5a477812c1980500576d1197c
gentiana <latem999@o2.pl>
2014-06-04 16:25:19 +0200

    Add user tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,18 +29,46 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      
+      describe "after submission" do
+        before { click_button submit }
+        
+        it { should have_title 'Sign up' }
+        it 'should have error explanation' do
+          within '#error_explanation' do
+            should have_content 'error'
+            should have_content 'The form contains'
+            should have_selector 'ul'
+          end
+        end
+        specify 'fields with errors should be inside right divs' do
+          within 'form#new_user' do
+            should have_selector 'div.field_with_errors', count: 6
+            should have_selector 'label[for=user_name]', text: 'Name'
+          end
+        end
+      end
     end
     
     describe "with valid information" do
+      let(:user_email) { 'mietekbylzajety@o2.pl' }
       before do
         fill_in "Name",         with: "Gryzelda Ciołek"
-        fill_in "Email",        with: "mietekbylzajety@o2.pl"
+        fill_in "Email",        with: user_email
         fill_in "Password",     with: "ala123"
         fill_in "Confirmation", with: "ala123"
       end
       
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: user_email) }
+        
+        it { should have_title user.name }
+        it { should have_selector 'div.alert.alert-success', text: 'Welcome' }
       end
     end
   end
